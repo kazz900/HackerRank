26f67418c9170acda1533789da6111fc4e8a1f54
Rara <135593141+DearRed@users.noreply.github.com>
2023-08-15 16:32:05 +0900

    @kazz900 asdf

--- rara/Helloworld/src/App.java
+++ rara/Helloworld/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        int [] stdin = new int[]{
+            42,
+            100,
+            125
+        };
+        for (int i = 0; i < stdin.length; i++) {
+            System.out.println(stdin[i]);
+        }
     }
 }
